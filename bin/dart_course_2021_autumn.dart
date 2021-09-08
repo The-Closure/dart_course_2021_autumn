1a046400f0738c669a4a15c98ef00c9abf8d242c
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-09-09 01:39:17 +0300

    init point

---
+++ bin/dart_course_2021_autumn.dart
@@ -0,0 +1,5 @@
+import 'package:dart_course_2021_autumn/dart_course_2021_autumn.dart' as dart_course_2021_autumn;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_course_2021_autumn.calculate()}!');
+}
