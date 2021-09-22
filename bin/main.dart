a354baa47acbbbd3e9e8cefa10a3c14a0675394d
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-09-22 21:53:33 +0300

    init point

--- bin/main.dart
+++ bin/main.dart
@@ -1,43 +1,18 @@
-import 'dart:math';
+import 'package:http/http.dart' as http;
 
 void main() {
-  print('start'); //1
-  Future<int> a = futureInt(); //3
-
-  a.then((value) {
+  requestClass.getData().then((value) {
     print(value);
-    return value;
-  }, onError: (e) {
-    print('error');
-  });
-  print('after future fun'); //2
-
-  Stream<int> stream = seqAsyncValues();
-  stream.listen((value) {
-    print(value);
-  }, onDone: () {
-    print('done');
-  }, onError: (e) {
-    print('error occured on $e');
-  }, cancelOnError: false);
-}
-
-Future<int> futureInt() {
-  return Future.delayed(Duration(seconds: 1), () async {
-    int a = await Random().nextInt(10);
-    if (a < 5)
-      throw Exception();
-    else
-      return a;
   });
 }
 
-Stream<int> seqAsyncValues() async* {
-  for (var i = 0; i < 10; i++) {
-    await Future.delayed(Duration(seconds: 1));
-    int a = await Random().nextInt(10);
-    String s = 'testtest';
-    s[a];
-    yield i;
+class requestClass {
+  static Future<String> getData() async {
+    var a = 3;
+    String date = '';
+    http.Response response = await http.get(Uri.parse(
+        'http://localhost:8080/api/v3/mirror/vj6j6spd0ibogcfr3mjan1jqp4/pics'));
+    print(response.statusCode);
+    return response.body;
   }
 }
